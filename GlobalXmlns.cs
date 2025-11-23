985242f929e0a07f5a10b3d9ecbfb83337d1f8b8
raulbancea <raul.bancea@stud.ubbcluj.ro>
2025-11-23 23:44:17 +0200

    Add project files.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BanceaRaulLab7")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BanceaRaulLab7.Pages")]
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BanceaRaulLab7
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
